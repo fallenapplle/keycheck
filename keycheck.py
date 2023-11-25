214d500314b841b9b210f9ef37f547e45ac95b90
fallenapplle <127190545+fallenapplle@users.noreply.github.com>
2023-11-25 20:19:34 +0500

    Update keycheck.py

--- keycheck.py
+++ keycheck.py
@@ -39,14 +39,35 @@ def check_google_maps(g_maps_url, verbose=False):
 
 # Put all urls in main function for  to make arg parser 
 #Dest is check the values in  nampesapce
+def google_recaptcha_key(api_key , verbose=False):
+    recapthca_url = "https://www.google.com/recaptcha/api/siteverify"
+    recaptcha_secret = api_key
+    recaptcha_response = "Response for test"
+
+
+    try:
+        response = requests.post(recapthca_url, data={"secret": recaptcha_secret, "response": recaptcha_response})
+        print("ReCaptcha Verification.")
+        print(response.json())
+        print("\n" + "="*50 + "\n")
+    except Exception as e:
+        print(f"Error making ReCAPTCHA request: {type(e).__name__} - {e}")
+        print("\n" + "="*50 + "\n")
+
+
 def main():
     parser = argparse.ArgumentParser(description="For api key check")
-    parser.add_argument("--g-map-key", "-key", dest="api_key", help="APi key for google maps", required=True)
+    parser.add_argument("-api-key",  dest="api_key", help="APi key for google maps", required=True)
+    parser.add_argument("--type", dest="key_type", help="Types of key (google_maps or recaptcha)", required=True)
     parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode. ")
 
     args = parser.parse_args()
 
-    google_maps_urls = [f'https://maps.googleapis.com/maps/api/staticmap?center=45%2C10&zoom=7&size=400x400&key={args.api_key}',
+    
+    
+    if args.key_type == "gm":
+        
+        google_maps_urls = [f'https://maps.googleapis.com/maps/api/staticmap?center=45%2C10&zoom=7&size=400x400&key={args.api_key}',
         f'https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key={args.api_key}',
         f'https://www.google.com/maps/embed/v1/place?q=place_id:ChIJyX7muQw8tokR2Vf5WBBk1iQ&key={args.api_key}',
         f'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key={args.api_key}',
@@ -58,11 +79,16 @@ def main():
         f'https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key={args.api_key}',
         f'https://roads.googleapis.com/v1/nearestRoads?points=60.170880,24.942795%7C60.170879,24.942796%7C60.170877,24.942796&key={args.api_key}'
         ]
-    # add args and des=api key to give key 
-    for urls in google_maps_urls:
-        check_google_maps(urls, verbose=args.verbose)
+        for url in google_maps_urls:
+            check_google_maps(url, verbose=args.verbose)
+    elif args.key_type == "rc":
+        google_recaptcha_key(args.api_key, verbose=args.verbose)
+    else:
+        print("Something went wrong. Invalid Supported type.")
+
 
 if __name__ == "__main__":
     main()
+
     
-    
+
